431218915c133b7e5a7a352144670f1d9f0ad78f
tima ali <timaali43@gmail.com>
2023-02-08 12:51:35 +0300

    This commit adds the POST fuctionality

--- routes/books.js
+++ routes/books.js
@@ -3,9 +3,35 @@ const model = require('./../models/library');
 
 const router = express.Router();
 
-router.get('/', function(req, res){
-  res.send("Get a list of books");
+// CRUD Operations
+//Get
+router.get('/', async function(req, res){
+    try {
+        const data = await model.find();
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
 });
 
-// CRUD Operations
+//POST
+router.post('/post', async (req, res) => {
+    const data = new model({
+        Book_name: req.body.Book_name,
+        Category: req.body.Category,
+        Author: req.body.Author,
+    })
+
+    try {
+        const dataToSave = await data.save();
+        res.status(200).json(dataToSave)
+    }
+    catch (error) {
+        res.status(400).json({message: error.message})
+    }
+})
+
+// DELETE
+
+
 module.exports=router;
